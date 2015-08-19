2f53c1df7d2d6b6cae53f6be788dc6e0aaa5d521
gui machiavelli <hey@guimachiavelli.com>
2015-08-19 17:28:35 +0200

    cms:foundation: finished template

--- content/themes/vac/foundation-template.php
+++ content/themes/vac/foundation-template.php
@@ -1,1 +1,18 @@
 <?php /* Template Name: Foundation page */ ?>
+<?php get_header(); ?>
+    <?php while (have_posts()): the_post(); ?>
+        <?php
+            $fields = get_fields($post->ID);
+            $fields = VACTemplate::parsed_ACF($fields);
+        ?>
+
+        <div class="column column--left">
+            <?php VACTemplate::ACF_loop($fields['left']); ?>
+        </div>
+
+        <div class="column column--right">
+            <?php VACTemplate::ACF_loop($fields['right']); ?>
+        </div>
+    <?php endwhile; ?>
+
+<?php get_footer(); ?>
