8e9ff570d147a359c6ddddc5bc5ba40f969f928e
gui machiavelli <hey@guimachiavelli.com>
2015-08-25 15:42:52 +0200

    styles:header: logo 👯, languages, vertical spacing

--- content/themes/vac/partials/nav-vac_languages.php
+++ content/themes/vac/partials/nav-vac_languages.php
@@ -1,10 +1,14 @@
 <?php $languages = VACNav::language(); ?>
 
-<ul>
-<?php foreach ($languages as $language): ?>
-    <?php if ($language['current_lang'] == true) { continue; } ?>
-    <li>
-        <a href="<?php echo $language['url']; ?>"><?php echo $language['name']; ?></a>
-    </li>
-<?php endforeach; ?>
-</ul>
+<nav class="nav-language">
+    <ul>
+    <?php foreach ($languages as $language): ?>
+        <?php if ($language['current_lang'] == true) { continue; } ?>
+        <li class="nav-language__item">
+            <a class="nav-language__link" href="<?php echo $language['url']; ?>">
+                <?php echo $language['name']; ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+</nav>
--- content/themes/vac/partials/nav-vac_primary.php
+++ content/themes/vac/partials/nav-vac_primary.php
@@ -2,8 +2,9 @@
     wp_nav_menu(array(
         'theme_location' => VACNav::$location,
         'container' => 'nav',
-        'container_class' => 'header-menu',
-        'menu_class' => 'header-menu-list',
-        'container_id' => 'header-menu',
+        'container_class' => 'nav-primary',
+        'menu_class' => 'nav-primary__list',
+        'container_id' => '',
+        'menu_id' => ' '
     ));
 ?>
