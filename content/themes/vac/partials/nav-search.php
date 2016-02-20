e76429271201b24ebd94dbf50977104d1c66f1eb
gui machiavelli <hey@guimachiavelli.com>
2016-02-20 15:35:53 +0100

    bugfix:search: link to english search no longer 404s

--- content/themes/vac/partials/nav-search.php
+++ content/themes/vac/partials/nav-search.php
@@ -1,7 +1,13 @@
 <?php
-    $search_language = pll_current_language();
+    //XXX: sorry future self, very ugly code
+    $default_language = pll_default_language();
+    $current_language = pll_current_language();
+    $search_language = $current_language != $default_language ?
+                                            $current_language  :
+                                            '';
     $search_text = $search_language == 'ru' ? 'Поиск' : 'Search';
-    $search_url = pll_home_url('en') . $search_language . '/?s=';
+    $search_url = pll_home_url($default_language) . $search_language;
+    $search_url .= $search_language == '' ? '?s=' : '/?s=';
 ?>
 
 <a class="search-link" href="<?php echo $search_url;  ?>">
